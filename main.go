ea55d36192ebbeb7ae626e1ffcbba60a60eefda1
Paul Maddox <pmaddox@amazon.com>
2018-03-26 20:06:00 +0100

    Switched URL to BBC

--- main.go
+++ main.go
@@ -4,29 +4,49 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 	"time"
 
 	"github.com/gorilla/mux"
 )
 
-var url = "http://ae35bcc3f30d911e88672066e78bb0cd-49118c41d965be0f.elb.us-west-2.amazonaws.com"
+// var url = "http://ae35bcc3f30d911e88672066e78bb0cd-49118c41d965be0f.elb.us-west-2.amazonaws.com"
+var url = "http://www.bbc.co.uk"
 var successes uint64
-var failures uint64
+var errors = []string{}
 
 func main() {
 
 	ticker := time.NewTicker(100 * time.Millisecond)
 	go func() {
 		for range ticker.C {
-			resp, err := http.Get(url)
-			if err != nil || resp.StatusCode != 200 {
-				atomic.AddUint64(&failures, 1)
+
+			timeout := time.Duration(5 * time.Second)
+			client := http.Client{Timeout: timeout}
+			resp, err := client.Get(url)
+
+			if err != nil {
+				errors = append(errors, fmt.Sprintf("ERROR: %s", err))
+				if len(errors) > 50 {
+					errors = errors[:50]
+				}
 				fmt.Println(0)
 				continue
 			}
+
+			if resp.StatusCode != 200 {
+				errors = append(errors, fmt.Sprintf("ERROR: non-200 status code (%d)", resp.StatusCode))
+				if len(errors) > 50 {
+					errors = errors[:50]
+				}
+				fmt.Println(0)
+				continue
+			}
+
 			atomic.AddUint64(&successes, 1)
 			fmt.Println(1)
+
 		}
 	}()
 
@@ -38,6 +58,7 @@ func main() {
 
 func statsHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
-	status := fmt.Sprintf("successes: %d, failures: %d", atomic.LoadUint64(&successes), atomic.LoadUint64(&failures))
+	status := strings.Join(errors, "\n")
+	status += fmt.Sprintf("\nsuccesses: %d, failures: %d\n", atomic.LoadUint64(&successes), len(errors))
 	w.Write([]byte(status))
 }
